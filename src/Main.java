253bbc9a45ad04b2ad21d5ef5f4affef506fbe1d
MariaSizz <buchacik@mail.ru>
2024-12-02 23:54:03 +0300

    Done2.

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,19 @@ class Main {
     public static void main(String[] args) {
         //1
         var firstName = "Ivan";
-        var middleName = "Petrov";
-        var lastName = "Petrovich";
-        var fullName = middleName + " " + firstName + " " + lastName;
-        System.out.println(fullName);
+        var middleName = "Ivanovich";
+        var lastName = "Ivanov";
+        var fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
 
 
         //2
-        System.out.println(fullName.toUpperCase());
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
 
         //3
-        fullName = "Петров Семён Петрович";
+        fullName = "Иванов Семён Семенович";
         fullName = fullName.replace("ё", "е" );
-        System.out.println(fullName);
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
         }
 
 
